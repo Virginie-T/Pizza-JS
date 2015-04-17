fbab33199d7c24196e73ce663394949fb480c7bf
virginie <virginietrubiano@gmail.com>
2015-04-17 11:17:01 -0700

    add more functionality :can choose size for pizza and changing the price

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,22 +1,34 @@
 
 $(document).ready(function() {
 
-
 var Pizza = {
-    price: "7$",
+    price: 7,
     initName: function() {
         this.name = "";
+    },
+    final_price: function(size) {
+      if (size === "small") {
+          this.price += 0;
+      } if (size === "medium") {
+        this.price += 3;
+      } if (size === "large") {
+        this.price += 6;
+      }
+      return this.price + "$";
     }
+
   }
 
     $("form#pizza").submit(function(event) {
         event.preventDefault();
 
         var pizza_name = $("select.pizza").val();
+        var pizza_size = $("select.size").val();
 
         var newPizza = Object.create(Pizza);
         newPizza.initName();
         newPizza.name = pizza_name;
+        newPizza.final_price(pizza_size);
 
         $('#result').show();
 

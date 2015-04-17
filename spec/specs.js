d129df6583b5f1c79679c381906740e6e5710e2b
virginie <virginietrubiano@gmail.com>
2015-04-17 09:59:47 -0700

    add specs for pepperoni pizza

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-    describe('cheese', function() {
-        it('will return the name of the pizza => "Cheese"', function() {
+    describe('cheese-pizza', function() {
+        it('will return the name of the cheese pizza => "Cheese"', function() {
 				var cheese = Object.create(Pizza);
 				cheese.initName();
 				cheese.name = "Cheese";
@@ -7,12 +7,32 @@
         });     
     });
 
-        describe('cheese', function() {
-        it('will return the name of the pizza => "Cheese"', function() {
+        describe('cheese-pizza', function() {
+        it('will return the price of the cheese pizza => "$5"', function() {
 				var cheese = Object.create(Pizza);
 				cheese.initPrice();
 				cheese.price = "$5";
                 expect(cheese.price).to.equal("$5");
         });     
     });
+
+            describe('pepperoni-pizza', function() {
+        it('will return the name of the pepperonni pizza => "Pepperoni"', function() {
+				var pepperoni = Object.create(Pizza);
+				pepperoni.initName();
+				pepperoni.name = "Pepperoni";
+                expect(pepperoni.name).to.equal("Pepperoni");
+        });     
+    });
+
+        describe('pepperoni-pizza', function() {
+        it('will return the price of the pepperoni pizza => "$7"', function() {
+				var pepperoni = Object.create(Pizza);
+				pepperoni.initPrice();
+				pepperoni.price = "$7";
+                expect(pepperoni.price).to.equal("$7");
+        });     
+    });
+
+
 
